2444723554e7bda4287beb636878ada077ef741f
Marsell Basi <your-email@example.com>
2026-08-23 13:36:48 -0400

    chore: register external Book BASI Studio

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -5,5 +5,8 @@ export default defineCliConfig({
     projectId: process.env.SANITY_STUDIO_PROJECT_ID || 'spjfohj1',
     dataset: process.env.SANITY_STUDIO_DATASET || 'production',
   },
+  deployment: {
+    appId: 'cfx2trz97tam1i1fgp9dxnhr',
+  },
   studioHost: process.env.SANITY_STUDIO_HOST || 'https://studio.bookbasi.com',
 })
